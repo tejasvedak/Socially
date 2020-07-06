36957f7334fb75462d7f5ac17ed111441ea9a409
Tejas Vedak <52121139+tejasvedak@users.noreply.github.com>
2020-07-06 18:06:38 +0530

    Add files via upload

---
+++ client/build/precache-manifest.71131ab9e2e87173c3c9ac9a05b44161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e959f7043ebd9066b690b54c658196d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70ed78a775268301c46b",
+    "url": "/static/css/main.98346d44.chunk.css"
+  },
+  {
+    "revision": "eca84a0f1b2d5b4b65aa",
+    "url": "/static/js/2.632b2d2b.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.632b2d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ed78a775268301c46b",
+    "url": "/static/js/main.e850025f.chunk.js"
+  },
+  {
+    "revision": "48a354fb5103cdff3976",
+    "url": "/static/js/runtime-main.12f1707d.js"
+  }
+]);
